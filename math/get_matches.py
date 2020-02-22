0f255ac512b61c39d599a08df7fbc1be7d8e203d
ashabooga <bcuellowolffe@gmail.com>
2020-02-22 17:33:55 -0500

    Working on finding which future and current events have matches listed, issues with call limit of the api

--- math/get_matches.py
+++ math/get_matches.py
@@ -2,20 +2,23 @@ import numpy as np
 import pandas as pd
 import requests
 import json
-from datetime import date
+from datetime import date, timedelta
+import time
 import sqlite3
 from sqlite3 import Error
 
 today = date.today()
+tdelta = timedelta(days=7)
+one_week_date = today + tdelta
 
 conn = sqlite3.connect('data.db') #Connecting to database
 c = conn.cursor()
 
-c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='events_1920'")
+c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='previous_events_1920'")
 
 #if the count is 1, then table exists
 if c.fetchone()[0]==1 :
-	previously_logged_events_1920_df = pd.read_sql_query("SELECT * FROM events_1920", conn)
+	previously_logged_events_1920_df = pd.read_sql_query("SELECT * FROM previous_events_1920", conn)
 else:
 	previously_logged_events_1920_df = pd.DataFrame()
 
@@ -24,8 +27,20 @@ event_request_1920 = requests.get(
     headers={'Content-Type': 'application/json', 'X-TOA-Key': 'ef98a4e91bcabfcc23d2241046f3894e3521ab605a30af96b2f0c6a30f0fdcdf', 'X-Application-Origin': 'roboDojo'},
 ) #Calling for all this year's events
 
-events_1920 = json.loads(event_request_1920.content)
+try: #COME BACK TO THIS IF WILL SAYS BETTER WAY
+	events_1920 = json.loads(event_request_1920.content)
+except:
+	print(event_request_1920)
+	time.sleep(15)
+
+	event_request_1920 = requests.get(
+		'https://theorangealliance.org/api/event?season_key=1920',
+    	headers={'Content-Type': 'application/json', 'X-TOA-Key': 'ef98a4e91bcabfcc23d2241046f3894e3521ab605a30af96b2f0c6a30f0fdcdf', 'X-Application-Origin': 'roboDojo'},
+	) #Calling for all this year's events
+	events_1920 = json.loads(event_request_1920.content)
+
 events_1920_df = pd.DataFrame(events_1920)
+
 
 indexNames = events_1920_df[(events_1920_df["event_type_key"] == "OTHER") | (events_1920_df["event_type_key"] == "SCRIMMAGE")].index
 events_1920_df.drop(indexNames , inplace=True) #Deleting certain types of events
@@ -47,10 +62,7 @@ events_1920_df = events_1920_df.drop(events_1920_df[events_1920_df.start_date >=
 events_1920_df = events_1920_df.reset_index()
 events_1920_df = events_1920_df.drop(columns=["index"])
 
-if previously_logged_events_1920_df.empty == True:
-	events_1920_df.to_sql("events_1920", con=conn, if_exists="replace")
-	updated_events_1920_df = events_1920_df
-else:
+if previously_logged_events_1920_df.empty == False:
 	indexList = []
 	for i in range(len(events_1920_df)):
 		eventKey = events_1920_df.loc[i, "event_key"]
@@ -60,25 +72,43 @@ else:
 			indexList.append(i)
 
 
-	updated_events_1920_df = previously_logged_events_1920_df
-	for i in indexList:
-		updated_events_1920_df.append(events_1920_df.loc[i])
+
+events_1920_df.to_sql("previous_events_1920", con=conn, if_exists="replace")
 
 
-	updated_events_1920_df = updated_events_1920_df.sort_values("start_date")
-	updated_events_1920_df = updated_events_1920_df.reset_index()
-	updated_events_1920_df = updated_events_1920_df.drop(columns=["index"])
-	try:
-		updated_events_1920_df = updated_events_1920_df.drop(columns=["level_0"])
-	except:
-		pass
+#MOVING ON TO LOOKING FOR USEABLE FUTURE EVENTS
 
-	updated_events_1920_df.to_sql("events_1920", con=conn, if_exists="replace")
+future_events_1920_df = future_events_1920_df.loc[future_events_1920_df["start_date"] <= str(one_week_date)]
+future_matches_df = pd.DataFrame(columns = list(events_1920_df))
 
-#STILL NEED TO DO: FIGURE OUT WHICH EVENTS TODAY + FUTURE HAVE ALL GAMES SCHEDULED
+for i in future_events_1920_df.event_key:
+	url = 'https://theorangealliance.org/api/{}/matches'.format(i)
+	while True:
+		try:
+			event_matches_request = requests.get(
+	    		url,
+	    		headers={'Content-Type': 'application/json', 'X-TOA-Key': 'ef98a4e91bcabfcc23d2241046f3894e3521ab605a30af96b2f0c6a30f0fdcdf', 'X-Application-Origin': 'roboDojo'},
+			)
+			event_matches = json.loads(event_matches_request.content)
+			break
+		except:
+			print(event_matches_request)
+			time.sleep(15)
+			event_matches_request = requests.get(
+		    	url,
+		    	headers={'Content-Type': 'application/json', 'X-TOA-Key': 'ef98a4e91bcabfcc23d2241046f3894e3521ab605a30af96b2f0c6a30f0fdcdf', 'X-Application-Origin': 'roboDojo'},
+			)
+			if str(event_matches_request) == "<Response [429]>":
+				print("still didn't work")
+			pass
 
-# for i in future_events_1920_df["event_key"]:
+	if str(event_matches) != "{'_code': 404, '_message': 'Content not found.'}":
+		event_matches_df = pd.DataFrame(event_matches)
+		future_matches_df.append(event_matches_df)
 
+print(future_matches_df.shape)
+print(future_events_1920_df.loc[(future_events_1920_df["start_date"] == min(future_events_1920_df.start_date)) and (future_events_1920_df["event_type_key"] == "QUAL")]["event_name"])
+print(min(future_events_1920_df.start_date))
 
 
 
